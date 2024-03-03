8a01fca653699acb90e8d9c2221376f3537864c5
Mustafa AZ <mustafa.22.abdulaziz@gmail.com>
2024-03-03 11:49:58 +0100

    initializing...

---
+++ src/test/java/com/mustafaaz/ecommerce/EcommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafaaz.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
